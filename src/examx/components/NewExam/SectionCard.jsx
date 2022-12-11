1d6b47fa62d50e3a1ad06603ee255a6f926e247f
javierorozco2 <eorozco8@ucol.mx>
2022-12-10 20:11:12 -0600

    new module: SectionCard... bugs fixed

---
+++ src/examx/components/NewExam/SectionCard.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react"
+import { useRef } from "react"
+import { AiOutlineClose } from "react-icons/ai"
+import { BiImageAdd, BiTrash } from "react-icons/bi"
+import { useDispatch } from "react-redux"
+import { addNewEmptyQuestion, removeDescImg, removeSection, setSectionDesc, setSectionTitle } from "../../../store/examx/examxSlices"
+import { addDescImg } from "../../../store/examx/thunks"
+import { QuestCard } from "./QuestCard"
+import BounceLoader from "react-spinners/BounceLoader"
+
+
+export const SectionCard = ({ key, info, secid }) => {
+
+    const imgref = useRef()
+    const [imgloader, setImgloader] = useState(false)
+    const dispatch = useDispatch()
+
+    // =============CAMBIAR TITULO DE SECCION============
+    const onHandleTitleSec = (data, id) => {
+        const { value } = data.target
+        dispatch(setSectionTitle({ value, id }))
+    }
+
+    // ============CAMBIAR DESCRIPCION DE SECCION===========
+    const onHandleDescSec = (data, id) => {
+        const { value } = data.target
+        dispatch(setSectionDesc({ value, id }))
+    }
+
+    // =========SUBIDA DE IMAGEN=========
+    const onFileInputChange = async ({ img, secid }) => {
+
+        if (img.target.files === 0) return
+
+        const file = img.target.files[0]
+
+        setImgloader(true)
+        await dispatch(addDescImg({ file, secid }))
+        setImgloader(false)
+
+        imgref.current.value = ''
+    }
+
+    return (
+        <div className="ne-section">
+
+            <div className='ne-section-contitle'>
+                <input
+                    placeholder='Ingresa un titulo de sección'
+                    className="ne-section-title"
+                    value={info.title}
+                    onChange={(e) => onHandleTitleSec(e, secid)}
+                />
+                <AiOutlineClose onClick={() => dispatch(removeSection(secid))} />
+            </div>
+
+            <div className='ne-section-divdesc'>
+                <textarea
+                    placeholder="Ingresa una descripción de sección"
+                    cols="30"
+                    rows="2"
+                    name='desc'
+                    value={info.desc}
+                    onChange={(e) => onHandleDescSec(e, secid)}
+                    className="ne-section-desc"
+                />
+
+                <input
+                    type="file"
+                    ref={imgref}
+                    onChange={(img) => onFileInputChange({ img, secid })}
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    disabled={imgloader}
+                />
+
+                {!imgloader ?
+                    <BiImageAdd
+                        onClick={() => imgref.current.click()}
+                        className='adddescimg-icon'
+                    />
+                    : <BounceLoader
+                        color="#3A89C9"
+                        speedMultiplier={.5}
+                        size={15}
+                    />
+                }
+
+            </div>
+
+            {
+                (info.image != '') &&
+                <div className='ne-section-image'>
+                    <img src={info.image} alt="" />
+                    <div className='animate__animated animate__fadeIn'>
+                        <BiTrash onClick={() => dispatch(removeDescImg(secid))} />
+                    </div>
+                </div>
+            }
+
+            {info.quest?.map((dat, id = key) => (
+
+                <QuestCard secid={secid} key={id} id={id} {...dat} />
+
+            ))}
+
+            <div className="ne-actbuttons">
+                <button onClick={() => dispatch(addNewEmptyQuestion({ secid }))}>+Pregunta cerrada</button>
+                {/* <button>+Pregunta abierta</button> EN CONSTRUCCIÓN */}
+            </div>
+        </div>
+    )
+}
--- src/examx/pages/NewExam.jsx
+++ src/examx/pages/NewExam.jsx
@@ -2,18 +2,14 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 
-import { AiOutlineClose, AiOutlineLeft } from 'react-icons/ai';
+import { AiOutlineLeft } from 'react-icons/ai';
 import { BsFillBookmarksFill } from "react-icons/bs"
 import { shortName } from '../helpers/shortName';
-import { addNewEmptyQuestion, addNewSection, onEditExam, removeDescImg, removeSection, setExamActiveEdit, setSectionDesc, setSectionTitle } from '../../store/examx/examxSlices';
+import { addNewSection, onEditExam, setExamActiveEdit } from '../../store/examx/examxSlices';
 
 import '../css/newexam.css'
-import { QuestCard } from '../components/NewExam/QuestCard';
 import { SaveExamModal } from '../components/NewExam/SaveExamModal';
-import { BiImageAdd, BiTrash } from 'react-icons/bi';
-import BounceLoader from "react-spinners/BounceLoader"
-import { useRef } from 'react';
-import { addDescImg } from '../../store/examx/thunks';
+import { SectionCard } from '../components/NewExam/SectionCard';
 
 
 const initialForm = {
@@ -50,59 +46,30 @@ export const NewExam = () => {
 
     const [formState, setFormState] = useState(initialForm)
     const [saveModal, setSaveModal] = useState(false)
-    const [imgloader, setImgloader] = useState(false)
 
     const { title, desc, egelRqst } = formState
 
     const { photoURL, displayName } = useSelector(state => state.auth)
     const { examActiveEdit, isloading } = useSelector(state => state.examx)
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const imgref = useRef()
 
     const activeModal = () => {
-
         setSaveModal(true)
         document.body.classList.add('no-scroll');
     }
 
     const onInputChange = ({ target }) => {
-
         const { name, value, checked } = target
-
         {
             value === 'on'
                 ? setFormState({ ...examActiveEdit, [name]: checked })
                 : setFormState({ ...examActiveEdit, [name]: value })
         }
-
     }
 
-    const onHandleTitleSec = (data, id) => {
-        const { value } = data.target
-        dispatch(setSectionTitle({ value, id }))
-    }
-
-    const onHandleDescSec = (data, id) => {
-        const { value } = data.target
-        dispatch(setSectionDesc({ value, id }))
-    }
-
-    // =========SUBIDA DE IMAGEN=========
-    const onFileInputChange = async ({ img, secid }) => {
-
-        if (img.target.files === 0) return
-
-        const file = img.target.files[0]
-
-        setImgloader(true)
-        await dispatch(addDescImg({ file, secid }))
-        setImgloader(false)
-
-        imgref.current.value = ''
-    }
     useEffect(() => {
-
         dispatch(setExamActiveEdit(formState))
     }, [formState])
 
@@ -146,74 +113,7 @@ export const NewExam = () => {
 
                 {
                     examActiveEdit.sections?.map((info, secid = key) => (
-                        <div className="ne-section" key={secid}>
-
-                            <div className='ne-section-contitle'>
-                                <input
-                                    placeholder='Ingresa un titulo de sección'
-                                    className="ne-section-title"
-                                    value={info.title}
-                                    onChange={(e) => onHandleTitleSec(e, secid)}
-                                />
-                                <AiOutlineClose onClick={() => dispatch(removeSection(secid))} />
-                            </div>
-
-                            <div className='ne-section-divdesc'>
-                                <textarea
-                                    placeholder="Ingresa una descripción de sección"
-                                    cols="30"
-                                    rows="2"
-                                    name='desc'
-                                    value={info.desc}
-                                    onChange={(e) => onHandleDescSec(e, secid)}
-                                    className="ne-section-desc"
-                                />
-
-                                <input
-                                    type="file"
-                                    ref={imgref}
-                                    onChange={(img) => onFileInputChange({ img, secid })}
-                                    accept="image/*"
-                                    style={{ display: 'none' }}
-                                    disabled={imgloader}
-                                />
-
-                                {!imgloader ?
-                                    <BiImageAdd
-                                        onClick={() => imgref.current.click()}
-                                        className='adddescimg-icon'
-                                    />
-                                    : <BounceLoader
-                                        color="#3A89C9"
-                                        speedMultiplier={.5}
-                                        size={15}
-                                    />
-                                }
-
-                            </div>
-
-                            {
-                                (info.image != '') &&
-                                <div className='ne-section-image'>
-                                    <img src={info.image} alt="" />
-                                    <div className='animate__animated animate__fadeIn'>
-                                        <BiTrash onClick={() => dispatch(removeDescImg(secid))} />
-                                    </div>
-                                </div>
-                            }
-
-                            {info.quest?.map((dat, id = key) => (
-
-                                <QuestCard secid={secid} key={id} id={id} {...dat} />
-
-                            ))}
-
-                            <div className="ne-actbuttons">
-                                <button onClick={() => dispatch(addNewEmptyQuestion({ secid }))}>+Pregunta cerrada</button>
-                                {/* <button>+Pregunta abierta</button> EN CONSTRUCCIÓN */}
-                            </div>
-                        </div>
-
+                        <SectionCard key={secid} info={info} secid={secid}/>
                     ))
                 }
 
